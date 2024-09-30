226711e5a4b080ff7095e0d914f962bc5503a6b7
Yash Sen <132945201+yaashhhh13@users.noreply.github.com>
2024-09-30 12:23:27 -0400

    test completed. All test cases passed

--- program1.js
+++ program1.js
@@ -2,8 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+    let stack = [];
+    let matchingBrackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (matchingBrackets[char]) {
+            stack.push(char);
+        }
+        else {
+            let topElement = stack.pop();
+            if (matchingBrackets[topElement] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
